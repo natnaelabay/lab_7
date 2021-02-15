6e59ede72655a0dd8c9de76f4e7d0952ea08ea19
Natnael Abay <se.natnael.abay@gmail.com>
2021-02-15 22:14:55 +0300

    [*fix] clock rotation degree adjusted

--- analog/assets/js/clock.js
+++ analog/assets/js/clock.js
@@ -4,24 +4,17 @@ const second = document.querySelector('.second');
 
 function getDate() {
     const dateNow = new Date()
-
     const nowHour = dateNow.getHours()
-
     const nowMinute = dateNow.getMinutes()
-    let nowSecond = dateNow.getSeconds()
-    if(nowSecond == 60) {
-        nowSecond = 1
-        console.log("one");
-    }
+    const nowSecond = dateNow.getSeconds()
 
-    const hourDegrees = (nowHour / 12) * 360 + 180 ;
-    const minuteDegrees = ((nowMinute / 60) * 360) + 180;
-    const secondDegrees = ((nowSecond / 60) * 360) + 180;
+    const hourDegrees = ((nowHour / 12) * 360) + ((nowMinute / 60) * 30) + 180;
+    const minuteDegrees = ((nowMinute / 60) * 360) + ((nowSecond / 60) * 6) + 180;
+    const secondDegrees = ((nowSecond / 60) * 360) + 90;
 
     hour.style.transform = `rotate(${hourDegrees}deg)`;
     minute.style.transform = `rotate(${minuteDegrees}deg)`;
     second.style.transform = `rotate(${secondDegrees}deg)`;
-
 }
 
 setInterval(getDate, 1000);
